03281e255da5fc4a063572bbac5007d85539bbf0
Muhammed Ali Kaya <86558862+Malipf@users.noreply.github.com>
2025-12-01 22:20:43 +0300

    Add files via upload

---
+++ day1.hs
@@ -0,0 +1,28 @@
+import System.Environment 
+
+rotate1 :: [String] -> Int -> Int -> ([String], Int, Int)
+rotate1 [] c state = ([], c, state)
+rotate1 (x:xs) c state =
+  let num    = read . tail $ x
+      isPlus = head x == 'R'
+      state2 = if isPlus then mod (state + num) 100 else mod (state - num) 100
+      c2     = if state2 == 0 then c + 1 else c
+   in rotate1 xs c2 state2
+
+rotate2 :: [String] -> Int -> Int -> ([String], Int, Int)
+rotate2 [] c state = ([], c, state)
+rotate2 (x:xs) c state =
+  let num    = read . tail $ x
+      isPlus = head x == 'R'
+      num2   = if isPlus then state + num else state - num
+      state2 = mod num2 100
+      c2     = if state2 == 0 then c + 1 else c
+      num3   = num2 `div` 100
+      c3     = c2 + abs num3
+      c4     = if num2 == 100 || (num2 < 0 && state == 0) then c3-1 else c3
+   in rotate2 xs c4 state2
+      
+main = do
+  path <- getArgs
+  contents <- readFile . head $ path
+  print ((\(a,b,c) -> b) (rotate2 (map (head . words) . lines $ contents) 0 50))
